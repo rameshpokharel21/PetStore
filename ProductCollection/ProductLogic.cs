efa219a73b1537c91c29893207e6e843092f2497
ramesh-pokharel <pokharelramesh@proton.me>
2024-03-23 18:00:25 -0400

    PetStore Part 2

---
+++ ProductCollection/ProductLogic.cs
@@ -0,0 +1,53 @@
+﻿namespace ProductCollection;
+
+internal class ProductLogic
+{
+    private List<Product> _products;
+    private Dictionary<string, DogLeash> _dogLeash;
+    private Dictionary<string, CatFood> _catFood;
+
+    public ProductLogic()
+    {
+        _products = new List<Product>();
+        _dogLeash = new Dictionary<string, DogLeash>();
+        _catFood = new Dictionary<string, CatFood>();
+    }
+
+    public void AddProduct(Product product)
+    {
+        _products.Add(product);
+
+        if(product is DogLeash)
+        {
+            _dogLeash.Add(key: product.Name, value: product as DogLeash);
+        }
+
+        if (product is CatFood)
+        {
+            _catFood.Add(key: product.Name, value: product as CatFood);
+        }
+    }
+
+    public List<Product> GetAllProducts()
+    {
+        return _products;
+    }
+
+    public DogLeash GetDogLeashByName(string name)
+    {
+        if(!_dogLeash.ContainsKey(name.ToLower()))
+        {
+            throw new ArgumentException($"Wrong name {name}");
+        }
+        return _dogLeash[name];
+    }
+
+    public CatFood GetCatFoodByName(string name)
+    {
+        if (!_catFood.ContainsKey(name.ToLower()))
+        {
+            throw new ArgumentException($"Wrong name {name}");
+        }
+        return _catFood[name];
+    }
+}
--- ProductCollection/Program.cs
+++ ProductCollection/Program.cs
@@ -8,8 +8,12 @@ if user enters other number or other letters or null
 default value will be 0.
 */
 
-Console.WriteLine("\nPress 1 to enter Product type of CatFood.");
-Console.WriteLine("Press 2 to enter Product type of DogLeash.");
+ProductLogic productLogic = new ProductLogic();
+Product? catFood;
+Product? dogLeash;
+
+Console.WriteLine("\nPress 1 to add a Product.");
+Console.WriteLine("Press 2 to print a product by name.");
 Console.WriteLine("Type 'exit' to quit.");
 
 string? userInput = Console.ReadLine() ?? "0";
@@ -39,74 +43,124 @@ while (userInput.ToLower() != "exit")
     It takes 0 if user enters other than integer type for Quantity Property
     */
 
-    Console.WriteLine("\nEnter name of the product:");
-    name = Console.ReadLine() ?? "no name";
-    Console.WriteLine($"The received value is {name}");
 
-    Console.WriteLine("\nEnter price for the product:");  
-    decimal.TryParse(Console.ReadLine(), out price);
-    Console.WriteLine($"The received value is {price}");
 
-    Console.WriteLine("\nEnter quantity as a whole number:");
-    int.TryParse(Console.ReadLine(), out quantity);
-    Console.WriteLine($"The received value is {quantity}");
 
-    Console.WriteLine("\nEnter description for the product:");
-    description = Console.ReadLine() ?? "no description";
-    Console.WriteLine($"The received value is {description}");
-
-    /*
-     CatFood class Properties input from user
-    and initializing and displaying CatFood object
-    if user enters non-numerial value for double, it takes 0
-    if user enters other than bool for bool type, it takes false
-    */
     if (convertedInput == 1)
     {
-        double weightPounds = 0;
-        bool kittenFood = false;
+        Console.WriteLine("\nType catfood or dogleash to add them:");
+        string? typeName = Console.ReadLine() ?? "none";
 
-        Console.WriteLine("\nEnter weight in pounds for cat food:");
-        double.TryParse(Console.ReadLine(), out weightPounds);
-        Console.WriteLine($"The received value is {weightPounds}");
+        if (typeName.ToLower() != "dogleash" && typeName.ToLower() != "catfood")
+        {
+            throw new ArgumentException($"Wrong prduct type: {typeName}");
+        }
 
-        Console.WriteLine("\nIs it kitten food(true or false)");
-        bool.TryParse(Console.ReadLine(), out kittenFood);
-        Console.WriteLine($"The received value is {kittenFood}");
+        Console.WriteLine("\nEnter name of the product:");
+        name = Console.ReadLine() ?? "no name";
+        Console.WriteLine($"The received value is {name}");
 
-        //creating CatFood object with given Properties
-        Product catFood = new CatFood(name, price, quantity, description, weightPounds, kittenFood);
+        Console.WriteLine("\nEnter price for the product:");
+        decimal.TryParse(Console.ReadLine(), out price);
+        Console.WriteLine($"The received value is {price}");
 
-        Console.WriteLine("\nHere is the cat food object: ");
-        Console.WriteLine(JsonSerializer.Serialize<CatFood>((CatFood)catFood));
+        Console.WriteLine("\nEnter quantity as a whole number:");
+        int.TryParse(Console.ReadLine(), out quantity);
+        Console.WriteLine($"The received value is {quantity}");
 
+        Console.WriteLine("\nEnter description for the product:");
+        description = Console.ReadLine() ?? "no description";
+        Console.WriteLine($"The received value is {description}");
 
+        /*
+         CatFood class Properties input from user
+        and initializing and displaying CatFood object
+        if user enters non-numerial value for double, it takes 0
+        if user enters other than bool for bool type, it takes false
+        */
+        if (typeName.ToLower().Equals("catfood"))
+        {
+            double weightPounds = 0;
+            bool kittenFood = false;
+
+            Console.WriteLine("\nEnter weight in pounds for cat food:");
+            double.TryParse(Console.ReadLine(), out weightPounds);
+            Console.WriteLine($"The received value is {weightPounds}");
+
+            Console.WriteLine("\nIs it kitten food(true or false)");
+            bool.TryParse(Console.ReadLine(), out kittenFood);
+            Console.WriteLine($"The received value is {kittenFood}");
+
+            //creating CatFood object with given Properties
+            catFood = new CatFood(name, price, quantity, description, weightPounds, kittenFood);
+
+           
+            productLogic.AddProduct((CatFood)catFood);
+
+            Console.WriteLine("\nThe CatFood product was added to the productLogic List\n");
+
+        }
+
+        /*
+         DogLeash class Properties entries from user
+        and initializing and displaying DogLeash object
+        if user enter other than int type for LengthInches, it takes 0
+        if string is null, it takes 'no material type' for Material Property
+        */
+        if (typeName.ToLower().Equals("dogleash"))
+        {
+            int lengthInches = 0;
+            string? material;
+
+            Console.WriteLine("\nEnter leash length in inches(whole number):");
+            int.TryParse(Console.ReadLine(), out lengthInches);
+            Console.WriteLine($"The received value is {lengthInches}");
+
+            Console.WriteLine("\nEnter material for the leash:");
+            material = Console.ReadLine() ?? "no material type";
+            Console.WriteLine($"The received value is {material}");
+
+            //create object with the values provided
+            dogLeash = new DogLeash(name, price, quantity, description, lengthInches, material);
+
+            
+            productLogic.AddProduct((DogLeash)dogLeash);
+
+            Console.WriteLine("\nThe DogLeash product was added to the productLogic List\n");
+        }
     }
 
-    /*
-     DogLeash class Properties entries from user
-    and initializing and displaying DogLeash object
-    if user enter other than int type for LengthInches, it takes 0
-    if string is null, it takes 'no material type' for Material Property
-    */
     if (convertedInput == 2)
     {
-        int lengthInches = 0;
-        string? material;
+        Console.WriteLine("\nPress 3 to enter catfood name or 4 to enter dogleash name to print the product:");
+        
+        if(int.TryParse(Console.ReadLine(), out int productType))
+        {
+            if (productType == 3)
+            {
+                Console.WriteLine("\nEnter cat-food name: ");
+                string? catFoodName = Console.ReadLine() ?? "";
+                Console.WriteLine("");
+                Console.WriteLine(JsonSerializer.Serialize(productLogic.GetCatFoodByName(catFoodName.ToLower())));
+            }
+            else if(productType == 4)
+            {
+                Console.WriteLine("\nEnter dog-leash name: ");
+                string? dogLeashName = Console.ReadLine() ?? "";
+                Console.WriteLine();
+                Console.WriteLine(JsonSerializer.Serialize(productLogic.GetDogLeashByName(dogLeashName.ToLower())));
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException($"Wrong type {productType}");
+            }
 
-        Console.WriteLine("\nEnter leash length in inches(whole number):");
-        int.TryParse(Console.ReadLine(), out lengthInches);
-        Console.WriteLine($"The received value is {lengthInches}");
+        }
 
-        Console.WriteLine("\nEnter material for the leash:");
-        material = Console.ReadLine() ?? "no material type";
-        Console.WriteLine($"The received value is {material}");
-
-        //create object with the values provided
-        Product dogLeash = new DogLeash(name, price, quantity, description, lengthInches, material);
-
-        Console.WriteLine("\nHere is the product:");
-        Console.WriteLine(JsonSerializer.Serialize<DogLeash>((DogLeash)dogLeash));
+        else
+        {
+            throw new ArgumentOutOfRangeException($"Wrong type {productType}");
+        }
     }
 
     Console.WriteLine("\nPress 1 to enter Product type of CatFood.");
@@ -114,6 +168,7 @@ while (userInput.ToLower() != "exit")
     Console.WriteLine("Type 'exit' to quit.");
 
     userInput = Console.ReadLine() ?? "0";
+
    
 }
 Console.WriteLine("\nBye bye!\n");
